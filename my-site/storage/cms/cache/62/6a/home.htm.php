d8e374e2210f1b6fe5ff441400caeb1cd6eeef42
Onege18 <Onege18.com>
2026-03-25 01:53:05 +0500

    feat: add mobile responsiveness and fix icon overflow in services section

--- my-site/storage/cms/cache/62/6a/home.htm.php
+++ my-site/storage/cms/cache/62/6a/home.htm.php
@@ -1,5 +1,5 @@
 <?php 
-class Cms69c2f3aaa5457424429469_a0d5080b875959a1a19c425c37b4afe7Class extends Cms\Classes\PageCode
+class Cms69c2f90ed2f47106732943_22ee152efcd16ef9ed78292947586a37Class extends Cms\Classes\PageCode
 {
 
 }
